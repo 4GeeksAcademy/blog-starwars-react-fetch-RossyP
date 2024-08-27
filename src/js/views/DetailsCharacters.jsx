0724e92c5aead28dd19c2fccb977719a20805903
Rossy Jasmin Palacin Nuñez <119419018+ROZZYNUEZ14@users.noreply.github.com>
2024-08-27 23:35:23 +0000

    Se agregan mas estilos a las vistas faltantes

--- src/js/views/DetailsCharacters.jsx
+++ src/js/views/DetailsCharacters.jsx
@@ -20,22 +20,25 @@ export const DetailsCharacters = () => {
     }, [id, store.detailCharacter, actions])
 
     return (
-        <div className="fondoPrincipal d-flex justify-content-center align-items-center vh-100">
-                <div className="d-flex py-3 bg-warning" style={{width: "60%"}}>
-                    <div className="d-flex justify-content-center align-items-center">
-                        <img src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`} onError={(e) => e.target.src = noImage} className="w-75"/> 
+        <div className="card bg-transparent d-flex justify-content-center align-items-center vh-100" style={{marginTop: "120px", marginBottom:"100px"}}>
+                <div className="row d-flex py-3 bg-warning" style={{width: "60%", borderRadius: "20px"}}>
+                    <div className="col-md-7 d-flex justify-content-center align-items-center">
+                        <img src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`} onError={(e) => e.target.src = noImage} className="w-100" style={{borderTopLeftRadius: "20px", borderBottomLeftRadius:"20px"}}/> 
                     </div>
-                    <div className="d-flex flex-column justify-content-center align-items-start">
-                        <h1 className="textPerson">{store.detailCharacter?.properties?.name}</h1>
-                        <p className="text">{store.detailCharacter?.description}</p>
-                        <p className="text"><span className="textCategory fw-bold">Hair color:</span> {store.detailCharacter?.properties?.hair_color}</p>
-                        <p className="text"><span className="textCategory fw-bold">Skin color:</span> {store.detailCharacter?.properties?.skin_color}</p>
-                        <p className="text"><span className="textCategory fw-bold">Eye color:</span> {store.detailCharacter?.properties?.eye_color}</p>
-                        <p className="text"><span className="textCategory fw-bold">Birth year:</span> {store.detailCharacter?.properties?.birth_year}</p>
-                        <p className="text"><span className="textCategory fw-bold">Gender:</span> {store.detailCharacter?.properties?.gender}</p>
-                        <p className="text"><span className="textCategory fw-bold">Homeworld:</span> {store.urlPlanet?.properties?.name}</p>
-                        <p className="text"><span className="textCategory fw-bold">Mass:</span> {store.detailCharacter?.properties?.mass}</p>
-                        <p className="text"><span className="textCategory fw-bold">Height:</span> {store.detailCharacter?.properties?.height}</p>
+                    <div className="col-md-5 d-flex flex-column justify-content-center align-items-start">
+                        <div className="card-body">
+                            <h1 className="textPerson">{store.detailCharacter?.properties?.name}</h1>
+                            <p className="text">{store.detailCharacter?.description}</p>
+                            <p className="text"><span className="textCategory fw-bold">Hair color:</span> {store.detailCharacter?.properties?.hair_color}</p>
+                            <p className="text"><span className="textCategory fw-bold">Skin color:</span> {store.detailCharacter?.properties?.skin_color}</p>
+                            <p className="text"><span className="textCategory fw-bold">Eye color:</span> {store.detailCharacter?.properties?.eye_color}</p>
+                            <p className="text"><span className="textCategory fw-bold">Birth year:</span> {store.detailCharacter?.properties?.birth_year}</p>
+                            <p className="text"><span className="textCategory fw-bold">Gender:</span> {store.detailCharacter?.properties?.gender}</p>
+                            <p className="text"><span className="textCategory fw-bold">Homeworld:</span> {store.urlPlanet?.properties?.name}</p>
+                            <p className="text"><span className="textCategory fw-bold">Mass:</span> {store.detailCharacter?.properties?.mass}</p>
+                            <p className="text"><span className="textCategory fw-bold">Height:</span> {store.detailCharacter?.properties?.height}</p>
+                        </div>
+                        
                     </div>
                     
                 </div>
--- src/js/views/DetailsPlanets.jsx
+++ src/js/views/DetailsPlanets.jsx
@@ -14,22 +14,25 @@ export const DetailsPlanets = () => {
         actions.detallePlanetas(id)
     },[])
     return(
-        <div className="fondoPrincipal d-flex justify-content-center align-items-center vh-100">
-                <div className="d-flex py-3 bg-warning" style={{width: "60%"}}>
-                    <div className="d-flex justify-content-center align-items-center">
-                        <img src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} onError={(e) => e.target.src = noImage} className="w-75"/> 
+        <div className="card bg-transparent d-flex justify-content-center align-items-center vh-100" style={{marginTop: "120px", marginBottom:"100px"}}>
+                <div className="row d-flex py-3 bg-warning" style={{width: "60%", borderRadius: "20px"}}>
+                    <div className="col-md-7 d-flex justify-content-center align-items-center">
+                        <img src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} onError={(e) => e.target.src = noImage} className="w-100" style={{borderTopLeftRadius: "20px", borderBottomLeftRadius:"20px"}}/> 
                     </div>
-                    <div className="d-flex flex-column justify-content-center align-items-start">
-                        <h1 className="textPerson">{store.detailPlanet?.properties?.name}</h1>
-                        {/* <p className="text">{store.detailPlanet?.description}</p> */}
-                        <p className="text"><span className="textCategory fw-bold">Diameter:</span> {store.detailPlanet?.properties?.diameter}</p>
-                        <p className="text"><span className="textCategory fw-bold">Rotation period:</span> {store.detailPlanet?.properties?.rotation_period}</p>
-                        <p className="text"><span className="textCategory fw-bold">Orbital period:</span> {store.detailPlanet?.properties?.orbital_period}</p>
-                        <p className="text"><span className="textCategory fw-bold">Gravity:</span> {store.detailPlanet?.properties?.gravity}</p>
-                        <p className="text"><span className="textCategory fw-bold">Population:</span> {store.detailPlanet?.properties?.population}</p>
-                        <p className="text"><span className="textCategory fw-bold">Climate:</span> {store.detailPlanet?.properties?.climate}</p>
-                        <p className="text"><span className="textCategory fw-bold">Terrain:</span> {store.detailPlanet?.properties?.terrain}</p>
-                        <p className="text"><span className="textCategory fw-bold">Surface water:</span> {store.detailPlanet?.properties?.surface_water}</p>
+                    <div className="col-md-5 d-flex flex-column justify-content-center align-items-start">
+                        <div className="card-body">
+                            <h1 className="textPerson">{store.detailPlanet?.properties?.name}</h1>
+                            {/* <p className="text">{store.detailPlanet?.description}</p> */}
+                            <p className="text"><span className="textCategory fw-bold">Diameter:</span> {store.detailPlanet?.properties?.diameter}</p>
+                            <p className="text"><span className="textCategory fw-bold">Rotation period:</span> {store.detailPlanet?.properties?.rotation_period}</p>
+                            <p className="text"><span className="textCategory fw-bold">Orbital period:</span> {store.detailPlanet?.properties?.orbital_period}</p>
+                            <p className="text"><span className="textCategory fw-bold">Gravity:</span> {store.detailPlanet?.properties?.gravity}</p>
+                            <p className="text"><span className="textCategory fw-bold">Population:</span> {store.detailPlanet?.properties?.population}</p>
+                            <p className="text"><span className="textCategory fw-bold">Climate:</span> {store.detailPlanet?.properties?.climate}</p>
+                            <p className="text"><span className="textCategory fw-bold">Terrain:</span> {store.detailPlanet?.properties?.terrain}</p>
+                            <p className="text"><span className="textCategory fw-bold">Surface water:</span> {store.detailPlanet?.properties?.surface_water}</p>
+                        </div>
+                        
                     </div>
                     
                 </div>
